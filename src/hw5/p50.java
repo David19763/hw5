5069debaa2384a73a825097a10b39932967d8415
unknown <t109360141@ntut.org.tw>
2022-12-15 20:41:30 +0800

    first commit

---
+++ src/hw5/p50.java
@@ -0,0 +1,24 @@
+package hw5;
+
+public class p50 {
+	public static void main(String[] args) {
+		
+		Car6 car1 = new Car6("一號車");
+		Thread th1 = new Thread(car1);
+		th1.start();
+		for(int i=0;i<5;i++) {
+			System.out.println("正在進行main()的工作");
+		}
+	}
+}
+class Car6 implements Runnable{
+	private String name;
+	public Car6(String nm){
+		name = nm;
+	}
+	public void run() {
+		for(int i=0;i<5;i++) {
+			System.out.println("正在進行"+name+"的處理");
+		}
+	}
+}
---
+++ src/pa/Sample5_2.java
@@ -0,0 +1,9 @@
+package pa;
+
+public class Sample5_2 {
+	public static void main(String[] args) {
+		Car1 car1 = new Car1();
+		car1.show();
+	}
+}
+
